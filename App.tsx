794290b92a34b3c1af666c0d1301ebd804fdf9f1
sharlq <52783044+sharlq@users.noreply.github.com>
2021-08-23 18:34:08 +0300

    added the register functionality nad configured firebase

--- App.tsx
+++ App.tsx
@@ -4,14 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; 
 import {createStackNavigator} from '@react-navigation/stack';
 import LoadingScreen from "./components/auth/Landing";
+import RegisterScreen from './components/auth/Regiter'
+import './firebaseConfig';
 
-const Stack = createStackNavigator();
+
 export default function App() {
-
+  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LoadingScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ firebaseConfig.ts
@@ -0,0 +1,31 @@
+import firebase from "firebase";
+import "dotenv/config";
+
+import {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTHDOMAIN,
+  REACT_APP_PROJECTID,
+  REACT_APP_STORAGEBUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENTID
+  //@ts-ignore
+} from "@env";
+
+//const result = dotenv.config({path:'./.env'})
+
+var firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain:REACT_APP_AUTHDOMAIN,
+  projectId: REACT_APP_PROJECTID,
+  storageBucket: REACT_APP_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENTID,
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebaseConfig;
